9d36706407c6d083377d46265050dff92a52e407
Gevaun <geniepinguin@gmail.com>
2024-10-07 14:25:08 -0500

    Refactor CSS and update dice component styles

--- src/components/dice.jsx
+++ src/components/dice.jsx
@@ -1,7 +1,12 @@
 export default function Dice(props) {
-    
   return (
-    <div className={props.value == 1 ? `flex justify-center items-center  text-2xl border border-green-400 rounded-lg p-4 bg-green-300 font-bold shadow-md` : `flex justify-center items-center text-2xl border border-zinc-50 rounded-lg p-4 font-bold shadow-md`}>
+    <div
+      className={
+        props.value == 1
+          ? `flex justify-center items-center text-2xl border border-green-400 rounded-lg px-5 py-3 bg-green-300 font-bold shadow-md`
+          : `flex justify-center items-center text-2xl border border-zinc-50 rounded-lg px-5 py-4 font-bold shadow-md`
+      }
+    >
       {props.value}
     </div>
   );
